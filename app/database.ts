ba900d177722038c65439b6e6c04eb3bf6119e35
MRMUtahir <mutahirkareem820@gmail.com>
2023-12-24 21:51:24 +0500

    issue solve

--- app/database.ts
+++ app/database.ts
@@ -8,30 +8,36 @@ export interface ITask extends Document {
     updatedAt: Date;
 }
 
-const taskSchema = new Schema<ITask>({
-    title: {
-        type: String,
-        maxlength: 255,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        enum: ['OPEN', 'IN_PROGRESS', 'CLOSE'],
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now,
-    },
-});
+let Task: mongoose.Model<ITask>;
 
-const Task = mongoose.model<ITask>('Task', taskSchema);
+try {
+    // Try to get the existing model
+    Task = mongoose.model<ITask>('Task');
+} catch (error) {
+    // If the model doesn't exist, define it
+    Task = mongoose.model<ITask>('Task', new Schema<ITask>({
+        title: {
+            type: String,
+            maxlength: 255,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ['OPEN', 'IN_PROGRESS', 'CLOSE'],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }, { timestamps: true }));
+}
 
 export default Task;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+
+"use client"
+import React, { useEffect } from "react";
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 
+
+
+
 const IssuesPage = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/todos");
+        const redata = await res.json();
+        console.log(redata, ">>>>>>>>>>>>>redata");
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <Button>
