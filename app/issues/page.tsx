1cb060b2280c4ce6b895100fe2118ca35a9b6949
MRMUtahir <mutahirkareem820@gmail.com>
2023-12-24 22:55:08 +0500

    map issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,19 +1,24 @@
 
 "use client"
-import React, { useEffect } from "react";
-import { Button } from "@radix-ui/themes";
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 
 
 
 
+// Import statements...
+
 const IssuesPage = () => {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch("http://localhost:3000/api/todos");
         const redata = await res.json();
         console.log(redata, ">>>>>>>>>>>>>redata");
+        setData(redata);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -21,12 +26,36 @@ const IssuesPage = () => {
 
     fetchData();
   }, []);
+
   return (
-    <div>
-      <Button>
+    <>
+      <div className="flex  justify-around items-center"><Button>
         <Link href={"/issues/new"}>New issue</Link>
-      </Button>
-    </div>
+      </Button></div>
+      <Button>All</Button>
+      <div>
+
+
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>createdAt</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {data.map((issue, index) => (
+              <Table.Row key={index}>
+                <Table.RowHeaderCell>{issue.title}</Table.RowHeaderCell>
+                <Table.Cell>{issue.status}</Table.Cell>
+                <Table.Cell>{issue.createdAt}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </div></>
   );
 };
 
